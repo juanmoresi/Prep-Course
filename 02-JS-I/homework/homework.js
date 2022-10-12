4e6af661e0ad52e734e05f00d238e7d6904b1eae
juanmoresi <114307596+juanmoresi@users.noreply.github.com>
2022-10-12 20:44:51 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -77,12 +77,18 @@ function tienenMismaLongitud(str1, str2) {
    
   }
 
-function menosQueNoventa() {
+function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num < 90) {
+    return true;  
+  } else {
+    return false;
+  } 
+
   
-}
+  } 
 
 function mayorQueCincuenta() {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta

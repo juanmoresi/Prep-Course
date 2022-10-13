a32edaec6f8ccb97815aef433583491d440b81fd
juanmoresi <114307596+juanmoresi@users.noreply.github.com>
2022-10-12 21:14:04 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -104,7 +104,7 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  return (x / y)
+  return x % y
   
 }
 
@@ -112,18 +112,22 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(num / 2 === 0) {
+  if(num % 2 === 0) {
     return true;
   } else {
     return false;
   }
 }
 
-function esImpar() {
+function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if(num % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function elevarAlCuadrado() {
